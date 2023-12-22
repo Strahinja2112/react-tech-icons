0402721e5b0995df84c19adb1452b5f66a52258a
Strahinja <111398879+Strahinja2112@users.noreply.github.com>
2023-12-22 21:00:44 +0100

    added more icons

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
-
-type Props = {};
-
-export default function StartingFunction({}: Props) {
-  return <div>StartingFunction</div>;
-}
+export * from './icons/C';
+export * from './icons/CSharp';
+export * from './icons/CSS';
+export * from './icons/HTML';
+export * from './icons/JS';
+export * from './icons/Next';
+export * from './icons/React';
+export * from './icons/Supabase';
+export * from './icons/Tailwind';
+export * from './icons/TS';
