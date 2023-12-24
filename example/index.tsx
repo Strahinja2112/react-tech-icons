1b8c2e6272200528ef9375081920190edebb8e4b
Strahinja <111398879+Strahinja2112@users.noreply.github.com>
2023-12-24 22:37:33 +0100

    added 22

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,12 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { Angular } from '../src/index';
 
 const App = () => {
   return (
     <div>
-      <Thing />
+      <Angular />
     </div>
   );
 };
